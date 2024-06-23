5eca616d4cc1564ad34dd94b0d2c667c1837b708
elikofi <elijahamoako72@gmail.com>
2024-06-23 14:23:42 +0000

    Created the LoginQuery Handler

---
+++ Application/Authentication/UserManagement/Queries/Login/LoginQueryHandler.cs
@@ -0,0 +1,19 @@
+﻿using Application.Authentication.Common;
+using ErrorOr;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Authentication.UserManagement.Queries.Login
+{
+    public class LoginQueryHandler : IRequestHandler<LoginQuery, ErrorOr<AuthenticationResult>>
+    {
+        public async Task<ErrorOr<AuthenticationResult>> Handle(LoginQuery request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
+++ Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7039cd905983f65bd54d10c023cca9d80ff1cc7e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4dbe64456714d0783977e3e7319396700a6751b6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
