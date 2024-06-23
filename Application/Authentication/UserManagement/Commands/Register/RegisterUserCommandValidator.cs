9923ad738659263337093ce5207bbae252f7b625
elikofi <elijahamoako72@gmail.com>
2024-06-23 14:50:28 +0000

    Few edits in the register command validator class

--- Application/Authentication/UserManagement/Commands/Register/RegisterUserCommandValidator.cs
+++ Application/Authentication/UserManagement/Commands/Register/RegisterUserCommandValidator.cs
@@ -13,7 +13,7 @@ namespace Application.Authentication.UserManagement.Commands.Register
         {
             RuleFor(x => x.FirstName).NotEmpty();
             RuleFor(x => x.LastName).NotEmpty();
-            RuleFor(x => x.Email).NotEmpty();
+            RuleFor(x => x.Email).NotEmpty().EmailAddress();
             RuleFor(x => x.PasswordHash).NotEmpty();
         }
     }
--- Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
+++ Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5eca616d4cc1564ad34dd94b0d2c667c1837b708")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+abb5576615e266a2bd7bc10165bb19dcd018bfbd")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
