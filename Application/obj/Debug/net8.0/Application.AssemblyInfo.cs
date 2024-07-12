1ad1990f38bdd7c3b931710cfb4e4d8f7226c57c
elikofi <elijahamoako72@gmail.com>
2024-07-12 20:06:16 +0000

    Created directories for the Make User a Super-User feature.

--- Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
+++ Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bbeb6df5cae68c599acc59fcd8fc8440dcaf074d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d71ed899291fa63943994a8e95a525ef187c13f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
